5e42986e3ef4abdf909f7318956a8ff50b192ddc
SOFe <sofe2038@gmail.com>
2019-06-04 22:07:28 +0800

    Renamed tables to follow a prefix

--- ModernEcon/resources/core/currency.mysql.sql
+++ ModernEcon/resources/core/currency.mysql.sql
@@ -3,14 +3,14 @@
 -- #    { create
 -- #        { currency
 -- #            * Creates the currency table
-CREATE TABLE currency (
+CREATE TABLE modernecon_currency (
 	id   INT PRIMARY KEY AUTO_INCREMENT,
 	name VARCHAR(255) UNIQUE
 );
 -- #        }
 -- #        { subcurrency
 -- #            * Creates the subcurrency table
-CREATE TABLE subcurrency (
+CREATE TABLE modernecon_subcurrency (
 	id           INT PRIMARY KEY AUTO_INCREMENT,
 	name         VARCHAR(255),
 	currency     INT,
@@ -19,7 +19,7 @@ CREATE TABLE subcurrency (
 	magnitude    INT,
 	UNIQUE KEY (currency, name),
 	UNIQUE KEY (currency, magnitude),
-	FOREIGN KEY (currency) REFERENCES currency(id)
+	FOREIGN KEY (currency) REFERENCES modernecon_currency(id)
 )
 	CHARACTER SET 'utf8mb4';
 -- #        }
@@ -27,38 +27,38 @@ CREATE TABLE subcurrency (
 -- #    { load-all
 -- #        { currency
 SELECT id, name
-FROM currency;
+FROM modernecon_currency;
 -- #        }
 -- #        { subcurrency
 SELECT id, name, currency, symbolBefore, symbolAfter, magnitude
-FROM subcurrency;
+FROM modernecon_subcurrency;
 -- #        }
 -- #    }
 -- #    { load
 -- #        { currency
 -- #            :id int
 SELECT name
-FROM currency
+FROM modernecon_currency
 WHERE id = :id;
 -- #        }
 -- #        { subcurrency
 -- #            :id int
 SELECT id, name, currency, symbolBefore, symbolAfter, magnitude
-FROM subcurrency
+FROM modernecon_subcurrency
 WHERE currency = :id;
 -- #        }
 -- #    }
 -- #    { get-id-by-name
 -- #        :name string
 SELECT id
-FROM currency
+FROM modernecon_currency
 WHERE name = :name;
 -- #    }
 -- #    { add
 -- #        { currency
 -- #            :name string
 -- #            * Adds a new currency
-INSERT INTO currency (name)
+INSERT INTO modernecon_currency (name)
 VALUES (:name);
 -- #        }
 -- #        { subcurrency
@@ -68,7 +68,7 @@ VALUES (:name);
 -- #            :symbolAfter string
 -- #            :magnitude int
 -- #            * Adds a new subcurrency
-INSERT INTO subcurrency (name, currency, symbolBefore, symbolAfter, magnitude)
+INSERT INTO modernecon_subcurrency (name, currency, symbolBefore, symbolAfter, magnitude)
 VALUES (:name, :currency, :symbolBefore, :symbolAfter, :magnitude);
 -- #        }
 -- #    }
