40b7f116a743aaa6fed44075cb3a08276acbf360
SOFe <sofe2038@gmail.com>
2019-06-28 23:37:33 +0800

    Added registry for Account(Owner)?Type

---
+++ ModernEconomy/src/ModernPlugins/ModernEconomy/Core/Account/AccountTypeRegistry.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * ModernEconomy
+ *
+ * Copyright (C) 2019 ModernPlugins
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+namespace ModernPlugins\ModernEconomy\Core\Account;
+
+final class AccountTypeRegistry{
+	/** @var AccountType[] */
+	private $registry = [];
+
+	public function register(AccountType $type) : void{
+		$this->registry[$type->getId()] = $type;
+	}
+
+	public function getId(string $id) : ?AccountType{
+		return $this->registry[$id] ?? null;
+	}
+
+	/**
+	 * @return AccountType[]
+	 */
+	public function getAll() : array{
+		return $this->registry;
+	}
+}
